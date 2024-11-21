169e51fef62bf691059bbb0fe16ab4851ea189d6
uNastyy_ <bouissouromain82@gmail.com>
2024-11-21 16:18:41 +0100

    hello world

---
+++ src/main/java/org/example/tpjavaspring/TpJavaspringApplication.java
@@ -0,0 +1,19 @@
+package org.example.tpjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TpJavaspringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TpJavaspringApplication.class, args);
+    }
+
+    @GetMapping("/bonjour")
+    public String hello() {
+        return "Bonjour le monde !";
+    }
+}
---
+++ src/test/java/org/example/tpjavaspring/TpJavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
